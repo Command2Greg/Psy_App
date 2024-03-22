e42d58ed703ed67bd2abb3ba404bff268a404366
Andrey Denisov <denandrey191@gmail.com>
2024-03-22 14:16:20 +0100

    PSYP-49/50: create buttons questions & login

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 export function Navigation({ navLinks }: {
   navLinks: {
     label: string
@@ -8,8 +6,8 @@ export function Navigation({ navLinks }: {
 }) {
 
   return (
-    <header className="flex grid grid-row-2 mb-10 ">
-      <div className='flex justify-normal grid grid-cols-3'>
+    <header className="flex mb-10 ">
+      <div className="flex grid-cols-2">
 
           {navLinks.map(link => (
             
@@ -18,9 +16,9 @@ export function Navigation({ navLinks }: {
               href={link.href}
             >
               <button class="rounded-full text-center text-slate-100 bg-gray-500 hover:bg-slate-300 active:bg-slate-100 focus:outline-none focus:ring focus:ring-slate-500 hover:text-gray-500 ...">
-              <div className="flex  mx-3">
-              {link.label}
-              </div>
+                <div className="flex  mx-3">
+                  {link.label}
+                </div>
               </button>
             </a>
             
@@ -28,9 +26,7 @@ export function Navigation({ navLinks }: {
    
 
       </div>
-      <div className="flex justify-center">
-        <Image src="/siteName.webp" width={350} height={90} alt="Picture of the author"/> 
-      </div>
+
 
       
     </header>
--- src/app/components/TheHeader.tsx
+++ src/app/components/TheHeader.tsx
@@ -1,18 +1,26 @@
 import { Navigation } from "./Navigation"
+import Image from 'next/image'
 
 export const navItems = [
-  { label: 'home', href: '/' },
-  { label: 'psyhologists', href: '/psyhologists' },
+  //{ label: 'psyhologists', href: '/psyhologists' },
   { label: 'questions', href: '/questions' },
+  { label: 'login', href: '/' },
 ]
 
 export function TheHeader() {
   return (
-    <div>
+    <div className="flex grid-cols-2 mt-6 ml-6 size-full">
    
-      <Navigation navLinks={navItems} />
+      <div>
+        <Navigation navLinks={navItems} />
+      </div>
+
+      <div className="flex justify-center size-full">
+        <Image src="/siteName.webp" width={350} height={90} alt="Picture of the author"/> 
+      </div>
       
     </div>
+    
     
     
   )
