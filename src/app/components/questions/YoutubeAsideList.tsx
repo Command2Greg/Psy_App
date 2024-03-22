9cc3e6f6eb985a1c7c4a366d9c4e250e42800cc5
Alex <casha.mironyuk.2001@gmail.com>
2024-03-22 14:22:19 +0200

    Installed library react-lazyload-youtube, updated youtube component

--- src/app/components/questions/YoutubeAsideList.tsx
+++ src/app/components/questions/YoutubeAsideList.tsx
@@ -1,26 +1,26 @@
+"use client";
+
+import Youtube from "react-lazyload-youtube";
+import "react-lazyload-youtube/dist/index.css";
+
 export default function YoutubeAsideList() {
     //Статика до возможности получить данные с firestore
     const videos = [
-        "https://www.youtube.com/embed/n1Y2VfJC6Ec?si=2pDI9BUxjgOwI7gX",
-        "https://www.youtube.com/embed/n1Y2VfJC6Ec?si=2pDI9BUxjgOwI7gX",
-        "https://www.youtube.com/embed/n1Y2VfJC6Ec?si=2pDI9BUxjgOwI7gX",
-        "https://www.youtube.com/embed/n1Y2VfJC6Ec?si=2pDI9BUxjgOwI7gX"
+        "mfxQy5A_tHs",
+        "mfxQy5A_tHs",
+        "mfxQy5A_tHs",
+        "mfxQy5A_tHs"
     ];
 
     const showVideos = videos.map((el, idx) =>
-        <>
-            <iframe
-                className="w-full aspect-video bg-black"
-                src={el}
-                allow="accelerometer; clipboard-write; gyroscope; encrypted-media; picture-in-picture; web-share"
-                allowFullScreen
-            />
+        <div key={idx}>
+            <Youtube className="w-full bg-black aspect-video" videoId={el} height="140px" />
             {idx + 1 !== videos.length && <hr className="w-[85%] my-3 mx-auto" />}
-        </>);
+        </div>);
 
     return (
         //Когда будет готов макет страницы вопросов, поменяйте w-[15%] на w-full
-        <aside className="w-[15%] py-3 px-4 rounded-xl shadow-[0px_10px_10px_10px_rgba(0,0,0,0.3)]">
+        <aside className="hidden lg:block max-w-[15%] py-3 px-4 rounded-xl shadow-[0px_10px_10px_10px_rgba(0,0,0,0.3)]">
             {showVideos}
         </aside>
     );
