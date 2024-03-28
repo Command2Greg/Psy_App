ad470fd626c1c81e35872d3ad7f2e95a9e1f42f5
Andrey Denisov <denandrey191@gmail.com>
2024-03-28 18:33:28 +0100

    PSYP-61/60/63/62/68: bug fix + add loginButton

---
+++ src/app/components/LoginButton.tsx
@@ -0,0 +1,14 @@
+export function Registration () {
+    return (
+      <div className="flex mb-10 rounded-xl gap-4">  
+            <a className="pr-20" href="/registration">
+              <button className="rounded-full text-center text-slate-50 bg-gray-500 hover:bg-white active:bg-slate-50 focus:outline-none focus:ring focus:ring-slate-500 hover:text-gray-500">
+                <div className="flex mx-3">
+                  Регистрация
+                </div>
+              </button>
+            </a>
+      </div>
+    )
+  }
+  
